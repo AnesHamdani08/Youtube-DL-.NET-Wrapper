7b5e634dfe92e2fd5e391abf433fe23a81145617
AnesHamdani08 <38377619+AnesHamdani08@users.noreply.github.com>
2021-06-25 17:10:01 +0200

    Add files via upload

---
+++ Source Files/YTDLWrapper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Anes08.YoutubeDL")>
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("YoutubeDL Wrapper For MuPlay")>
+<Assembly: AssemblyCopyright("Copyright © Anes08 2021 - Comercial Use Is Prohibited")>
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3f580dc9-141c-4c18-9191-257d85167481")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
